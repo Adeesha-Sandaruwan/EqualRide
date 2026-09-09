a43fedd25847c6468a19990c2bcb5fc1a763e848
De Silva Y A S <it23551084@my.sliit.lk>
2026-09-09 09:51:20 +0530

    feat: save display accessibility settings

--- lib/models/accessibility_preferences.dart
+++ lib/models/accessibility_preferences.dart
@@ -3,12 +3,16 @@ class AccessibilityPreferences {
   final bool lowCrowding;
   final bool prioritySeating;
   final bool accessibilityAlerts;
+  final bool highContrast;
+  final double textScale;
 
   const AccessibilityPreferences({
     this.stepFreeRoutes = true,
     this.lowCrowding = false,
     this.prioritySeating = true,
     this.accessibilityAlerts = true,
+    this.highContrast = false,
+    this.textScale = 1.0,
   });
 
   Map<String, dynamic> toMap() {
@@ -17,15 +21,23 @@ class AccessibilityPreferences {
       'lowCrowding': lowCrowding,
       'prioritySeating': prioritySeating,
       'accessibilityAlerts': accessibilityAlerts,
+      'highContrast': highContrast,
+      'textScale': textScale,
     };
   }
 
   factory AccessibilityPreferences.fromMap(Map<String, dynamic> map) {
+    final savedTextScale = map['textScale'];
+
     return AccessibilityPreferences(
       stepFreeRoutes: map['stepFreeRoutes'] ?? true,
       lowCrowding: map['lowCrowding'] ?? false,
       prioritySeating: map['prioritySeating'] ?? true,
       accessibilityAlerts: map['accessibilityAlerts'] ?? true,
+      highContrast: map['highContrast'] ?? false,
+      textScale: savedTextScale is num
+          ? savedTextScale.toDouble()
+          : 1.0,
     );
   }
 }
